885f5a694da5b63d5240260cd246f34992edb342
matheusknaul <contatomatheush0909@gmail.com>
2025-05-13 23:56:30 -0300

    initial commit

---
+++ src/main/java/com/matheusknaul/Labsystem/LabsystemApplication.java
@@ -0,0 +1,13 @@
+package com.matheusknaul.Labsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusknaul/Labsystem/LabsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusknaul.Labsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
